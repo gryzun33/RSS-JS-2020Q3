1cbf0234ec8c4c6a76083cf14ad364bf7ab88157
gryzun33 <yakushevao777@gmail.com>
2020-10-21 12:45:17 +0300

    feat: add part1 of task

--- momentum/script.js
+++ momentum/script.js
@@ -1,11 +1,49 @@
 // DOM Elements
 const time = document.getElementById('time');
+const day = document.getElementById('day');
 const greeting = document.getElementById('greeting');
 const name = document.getElementById('name');
 const focus = document.getElementById('focus');
 
+const days = {
+  '0' : 'Sunday',
+  '1' : 'Monday',
+  '2' : 'Tuesday',
+  '3' : 'Wednesday',
+  '4' : 'Thursday',
+  '5' : 'Friday',
+  '6' : 'Saturday'
+};
+
+const months = {
+  '0' : 'January',
+  '1' : 'February',
+  '2' : 'March',
+  '3' : 'April',
+  '4' : 'May',
+  '5' : 'June',
+  '6' : 'July',
+  '7' : 'August',
+  '8' : 'September',
+  '9' : 'October',
+  '10' : 'November',
+  '11' : 'December'
+};
+
 // options 
-const showAmPM = true;
+// const showAmPM = true;
+
+// show date
+function showDate() {
+
+  let today = new Date();
+  let dayOfWeek = today.getDay().toString();
+  let number = today.getDate();
+  let month = today.getMonth().toString();
+
+  day.innerHTML = `${days[dayOfWeek]}, ${number} ${months[month]}`;
+  setTimeout(showDate, 1000);
+} 
 
 // Show time
 function showTime() {
@@ -16,14 +54,14 @@ function showTime() {
   let sec = today.getSeconds();
 
   // set PM or AM
-  const amPm = hour >= 12 ? 'PM' : 'AM';
+  // const amPm = hour >= 12 ? 'PM' : 'AM';
 
   // 12hr Format
-  hour = hour % 12 || 12;
+  // hour = hour % 12 || 12;
 
   // output time
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPM ? amPm : ''}`;
-
+  // time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPM ? amPm : ''}`;
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)}`;
   setTimeout(showTime, 1000);
 
 }
@@ -32,6 +70,8 @@ function showTime() {
 function addZero(n) {
   return (parseInt(n,10) < 10 ? '0' : '') + n;
 }
+
+
 
 // set background and greeting 
 function setBgGreet() {
@@ -47,7 +87,7 @@ function setBgGreet() {
 
   
   } else if (hour < 12) {
-    document.body.style.backgroundImage = "url('assets/images/morning/03.jpg')";
+    document.body.style.backgroundImage = "url('assets/images/morning/06.jpg')";
     greeting.textContent = 'Good Morning';
 
 
@@ -70,6 +110,7 @@ function getName() {
   if(localStorage.getItem('name') === null) {
     name.textContent = '[Enter Name]';
   } else {
+ 
     name.textContent = localStorage.getItem('name');
   }
 }
@@ -79,11 +120,28 @@ function getName() {
 function setName(e) {
   if(e.type === 'keypress') {
     if(e.which === 13 || e.code === 13) {
+
+
+      if(e.target.innerText.trim() === '') {
+        getName();
+        name.blur();
+      } else {
+
+
       localStorage.setItem('name', e.target.innerText);
       name.blur();
+      }
     }
   } else {
-    localStorage.setItem('name', e.target.innerText);
+
+    if(e.target.innerText.trim() === '') {
+      getName();
+    } else {
+    
+
+
+     localStorage.setItem('name', e.target.innerText);
+    }
   }
 }
 
@@ -116,8 +174,23 @@ name.addEventListener('blur', setName);
 focus.addEventListener('keypress', setFocus);
 focus.addEventListener('blur', setFocus);
 
+name.addEventListener('focus', function() {
+ 
+  name.textContent = ' ';
+  name.focus();
+  
+});
+
+// name.addEventListener('click', function() {
+//   name.textContent = ' ';
+// });
+
+
 
 // run
+
+showDate();
+
 showTime();
 
 setBgGreet();
