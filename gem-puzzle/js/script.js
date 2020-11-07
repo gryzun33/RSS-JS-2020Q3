3a036eaa6cf55b1782fc587cc3d9fb5bb5652fa6
gryzun33 <yakushevao777@gmail.com>
2020-11-07 16:44:08 +0300

    feat: add some functions

--- gem-puzzle/js/script.js
+++ gem-puzzle/js/script.js
@@ -3,12 +3,75 @@ const menu = document.createElement('ul');
 const currentGame = document.createElement('div');
 const container = document.createElement('div');
 const puzzleBox = document.createElement('div');
-const menuItemsNames = ['New game', 'Save', 'Best scores', 'Settings', 'Rules'];
+const menuItemsNames = ['New game', 'Saved game', 'Best scores', 'Settings'];
 const time = document.createElement('div');
 const moves = document.createElement('div');
 const pause = document.createElement('div');
 const blackout = document.createElement('div');
+const gameOver = document.createElement('div');
+let scores = [];
+// let bestScores = [];
 
+const settingsField = document.createElement('div');
+settingsField.classList.add('settings-hide');
+
+settingsField.innerHTML = `
+  <div class="field-size">
+    <div>Field size</div>
+    <br>
+    <form>
+      <select id="select" size="1">
+        <option value="3х3">3х3</option>
+        <option value="4x4">4х4</option>
+        <option value="5x5">5х5</option>
+        <option value="6x6">6х6</option>
+        <option value="7x7">7х7</option>
+        <option value="8x8">8х8</option>
+      </select>
+    </form>
+  </div>  
+
+  
+  <div id="start">Start new game</div>
+  
+  <div id="back">Back and continue</div>`
+;
+
+const pauseField = document.createElement('div');
+pauseField.classList.add('pause-hide');
+
+pauseField.innerHTML = `
+  <div id="saveGameBtn">Save Game</div>
+  <div id="backAndContinue">Back and Continue</div>`
+;  
+
+gameOver.classList.add('congrat-hide');
+// gameOver.innerHTML = 'Congratulations!';
+
+
+let totalTime;
+let score;
+const records = document.createElement('div');
+// records.classList.add('records-show');
+records.classList.add('records-hide');
+  
+  
+
+const cellCount = {
+  '3х3': { number: 9, width: 33.333 } ,
+  '4x4': { number: 16, width: 25 } ,
+  '5x5': { number: 25, width: 20 } ,
+  '6x6': { number: 36, width: 16.666 } ,
+  '7x7': { number: 49, width: 14.285 } ,
+  '8x8': { number: 64, width: 12.5 } ,
+}
+
+
+let count = cellCount['4x4'].number;
+
+let widthCell = cellCount['4x4'].width;
+
+let currentCellCount = '4x4';
 
 moves.classList.add('moves');
 wrapper.classList.add('wrapper');
@@ -16,8 +79,14 @@ menu.classList.add('menu');
 currentGame.classList.add('current');
 puzzleBox.classList.add('puzzle-box');
 blackout.classList.add('blackout-show');
-container.classList.add(blackout);
-container.classList.add(puzzleBox);
+container.classList.add('container');
+container.append(blackout);
+container.append(puzzleBox);
+container.append(settingsField);
+container.append(pauseField);
+container.append(gameOver);
+container.append(records);
+// console.log (select.selectedIndex);  
 
 let movesCount = 0;
 let minStart = '00';
@@ -38,20 +107,22 @@ menuItemsNames.forEach((elem) => {
 });
 
 const newGameBtn = menu.querySelector('li:nth-child(1)');
-const saveBtn = menu.querySelector('li:nth-child(2)');
+const savedGameBtn = menu.querySelector('li:nth-child(2)');
 const bestScoresBtn = menu.querySelector('li:nth-child(3)');
 const settingsBtn = menu.querySelector('li:nth-child(4)');
-const RulesBtn = menu.querySelector('li:nth-child(5)');
+// const RulesBtn = menu.querySelector('li:nth-child(5)');
 
-console.log(RulesBtn);
+// console.log(RulesBtn);
 
 wrapper.append(menu);
 wrapper.append(currentGame);
-wrapper.append(puzzleBox);
+wrapper.append(container);
 
 document.body.append(wrapper);
 
-console.log(wrapper);
+// console.log(wrapper);
+
+
 
 
 let randomArray;
@@ -64,16 +135,133 @@ let orderMemory;
 let startDate;
 let currentDate;
 let diff;
+let puzzle;
 
-newGameBtn.addEventListener('click', createNewGame);
 
-function createNewGame() {
+newGameBtn.addEventListener('click', () => {
+  createNewGame(count);
+});
+
+pause.addEventListener('click', () => {
+  blackout.classList.remove('blackout-hide');
+  blackout.classList.add('blackout-show');
+  pauseField.classList.add('pause-show');
+  pauseField.classList.remove('pause-hide');
+  clearInterval(timerId);
+});
+
+saveGameBtn.addEventListener('click', () => {
+  saveGame();
+});
+
+savedGameBtn.addEventListener('click', () => {
+  if (localStorage.getItem('savedPuzzle')) {
+    puzzle = JSON.parse(localStorage.getItem('savedPuzzle'));
+    puzzleBox.innerHTML = puzzle['puzzleBox'];
+    movesCount = puzzle['currentMoves'];
+    moves.innerHTML = `Moves ${movesCount}`;
+    min = puzzle['min'];
+    sec = puzzle['sec'];
+    blackout.classList.add('blackout-hide');
+    blackout.classList.remove('blackout-show');
+    chips = puzzleBox.querySelectorAll('.chip');
+    empty = puzzleBox.querySelector('.empty');
+    runTimer();
+    chipsHandler();
+  }
+});
+
+
+
+backAndContinue.addEventListener('click', () => {
+  blackout.classList.add('blackout-hide');
+  blackout.classList.remove('blackout-show');
+  pauseField.classList.remove('pause-show');
+  pauseField.classList.add('pause-hide');
+  runTimer();
+});
+
+start.addEventListener('click', () => {
+  createNewGame(count);
+});
+
+back.addEventListener('click', () => {
+  blackout.classList.add('blackout-hide');
+  blackout.classList.remove('blackout-show');
+  settingsField.classList.remove('settings-show');
+  settingsField.classList.add('settings-hide');
+  runTimer();
+});
+
+settingsBtn.addEventListener('click', () => {
+  blackout.classList.remove('blackout-hide');
+  blackout.classList.add('blackout-show');
+  settingsField.classList.add('settings-show');
+  settingsField.classList.remove('settings-hide');
+  select.value = currentCellCount;
+  // clearTimeout(timerId);
+  clearInterval(timerId);
+
+});
+
+
+
+
+select.addEventListener('change', () => {
+  console.log (`select.value = ${select.value}`);
+  count = cellCount[select.value].number;
+  console.log(count);
+  widthCell = cellCount[select.value].width;
+  console.log(widthCell);
+}); 
+// localStorage.setItem('bestScores', JSON.stringify(scores));
+
+bestScoresBtn.addEventListener('click', () => {
+  console.log('click best scores');
+  blackout.classList.remove('blackout-hide');
+  blackout.classList.add('blackout-show');
+
+  records.classList.add('records-show');
+  records.classList.remove('records-hide');
+
+  gameOver.classList.add('congrat-hide');
+  gameOver.classList.remove('congrat-show');
+  records.innerHTML = '';
+  
+  if (localStorage.getItem('bestScores')) {
+    scores = JSON.parse(localStorage.getItem('bestScores'))
+  }
+  console.log (`length  = ${scores.length}`);
+  for (let i = 0; i < scores.length; i++) {
+    let record = document.createElement('div');
+    record.innerHTML = `${i + 1}. time: ${scores[i]['time']}   moves: ${scores[i]['moves']}`;
+    records.append(record);
+  }
+
+});
+
+function createNewGame(n) {
+  clearInterval(timerId);
+  currentCellCount = `${Math.sqrt(n)}x${Math.sqrt(n)}`;
+  blackout.classList.add('blackout-hide');
+  blackout.classList.remove('blackout-show');
+  settingsField.classList.remove('settings-show');
+  settingsField.classList.add('settings-hide');
+  pauseField.classList.remove('pause-show');
+  pauseField.classList.add('pause-hide');
+  records.classList.remove('records-show');
+  records.classList.add('records-hide');
+  gameOver.classList.remove('congrat-show');
+  gameOver.classList.add('congrat-hide');
   puzzleBox.innerHTML = '';
+  time.innerHTML = `Time ${minStart} : ${secStart}`;
   movesCount = 0;
+  min = 0;
+  sec = 0;
   moves.innerHTML = `Moves ${movesCount}`;
   randomArray = [];
-  while (randomArray.length < 16) {
-    currentNumber = Math.floor((Math.random() * 16));
+  while (randomArray.length < n) {
+    currentNumber = Math.floor((Math.random() * n));
     if (!randomArray.includes(currentNumber)) {
       randomArray.push(currentNumber);
     }
@@ -85,6 +273,7 @@ function createNewGame() {
     currentChip = document.createElement('div');
     if (randomArray[i] !== 0) {
       currentChip.classList.add('chip');
+     
       currentChip.textContent = randomArray[i];
       currentChip.style.order = currentOrder;
       currentOrder++;
@@ -93,34 +282,49 @@ function createNewGame() {
       currentChip.style.order = currentOrder;
       currentOrder++;
     }
+    currentChip.style.width = `${widthCell}%`;
+    currentChip.style.height = `${widthCell}%`;
     puzzleBox.append(currentChip);
   }
+
+  
   
   
   chips = puzzleBox.querySelectorAll('.chip');
   empty = puzzleBox.querySelector('.empty');
-  startDate = new Date();
-  runTimer();
-  chipsHandler();
+
+
+  checkForSolve();
+  // startDate = new Date();
+  // runTimer();
+  // chipsHandler();
 
 }
 
+let timerId;
+let min = 0;
+let sec = 0;
 
 
 function runTimer() {
-  let currentDate = new Date();
 
-  let diff = currentDate.getTime() - startDate.getTime();
-  console.log (diff);
-  let min = Math.floor(diff / 60000);
-  let sec = Math.floor((diff % 60000) / 1000);
+  timerId = setInterval(() => {
+    if (sec === 59) {
+      min = min + 1;
+      sec = 0;
+    } else {
+      sec = sec + 1;
+    }
 
-  min = (parseInt(min, 10) < 10 ? '0' : '') + min;
-  sec = (parseInt(sec, 10) < 10 ? '0' : '') + sec;
-  time.innerHTML = `Time ${min} : ${sec}`;
+    currMin = (parseInt(min, 10) < 10 ? '0' : '') + min;
+    currSec = (parseInt(sec, 10) < 10 ? '0' : '') + sec;
+    time.innerHTML = `Time ${currMin} : ${currSec}`;
+  } , 1000);
 
-  setTimeout(runTimer, 1000);
+
 }
+
+
 
 
 
@@ -142,8 +346,9 @@ function chipsHandler() {
           empty.style.order = chip.style.order;
           chip.style.order = orderMemory;
           chip.classList.remove('to-top');
-          empty.classList.remove('to-bottom');        
-        },1000);       
+          empty.classList.remove('to-bottom');
+          isEnd(); 
+        },500);
       }
 
       if (chip.getBoundingClientRect().bottom === empty.getBoundingClientRect().top && chip.getBoundingClientRect().left === empty.getBoundingClientRect().left) {
@@ -156,8 +361,9 @@ function chipsHandler() {
           empty.style.order = chip.style.order;
           chip.style.order = orderMemory;
           chip.classList.remove('to-bottom');
-          empty.classList.remove('to-top');        
-        },1000);       
+          empty.classList.remove('to-top');
+          isEnd();        
+        },500);       
       }
 
       if (chip.getBoundingClientRect().left === empty.getBoundingClientRect().right && chip.getBoundingClientRect().top === empty.getBoundingClientRect().top) {
@@ -170,8 +376,9 @@ function chipsHandler() {
           empty.style.order = chip.style.order;
           chip.style.order = orderMemory;
           chip.classList.remove('to-left');
-          empty.classList.remove('to-right');        
-        },1000);       
+          empty.classList.remove('to-right');
+          isEnd();        
+        },500);       
       }
 
       if (chip.getBoundingClientRect().right === empty.getBoundingClientRect().left && chip.getBoundingClientRect().top === empty.getBoundingClientRect().top) {
@@ -184,8 +391,9 @@ function chipsHandler() {
           empty.style.order = chip.style.order;
           chip.style.order = orderMemory;
           chip.classList.remove('to-right');
-          empty.classList.remove('to-left');        
-        },1000);       
+          empty.classList.remove('to-left');
+          isEnd();        
+        },500);       
       }
 
       // console.log (chips);
@@ -194,6 +402,112 @@ function chipsHandler() {
 }
 
 
+
+function saveGame() {
+
+  puzzle = {
+    'puzzleBox': puzzleBox.innerHTML,
+    'min': min,
+    'sec': sec,
+    // 'currentTimer': time.innerHTML,
+    'currentMoves': movesCount,
+  }
+  
+  // localStorage.setItem('puzzleBox', puzzleBox.innerHTML );
+  // localStorage.setItem('currentTimer', time.innerHTML );
+  // localStorage.setItem('currentMoves', movesCount );
+  localStorage.setItem('savedPuzzle', JSON.stringify(puzzle));
+  pauseField.classList.remove('pause-show');
+  pauseField.classList.add('pause-hide');
+  movesCount = 0;
+  moves.innerHTML = `Moves ${movesCount}`;
+  min = 0;
+  sec = 0;
+  currMin = (parseInt(min, 10) < 10 ? '0' : '') + min;
+  currSec = (parseInt(sec, 10) < 10 ? '0' : '') + sec;
+  time.innerHTML = `Time ${currMin} : ${currSec}`;
+  
+}
+
+function isEnd() {
+  for (let i = 0; i < chips.length; i++) {
+    console.log(chips[i].style.order);
+    if (+chips[i].style.order !== +chips[i].innerHTML) {
+      console.log('non equal');
+      return;
+    } else continue;
+  }
+  clearInterval(timerId);
+  gameOver.innerHTML = `You solved gem-puzzle in ${currMin} : ${currSec} and ${movesCount} moves!!!`;
+  gameOver.classList.add('congrat-show');
+  gameOver.classList.remove('congrat-hide');
+  addBestScore();
+}
+
+
+function checkForSolve() {
+  let counter = 0;
+  for (let i = 0; i < chips.length - 1; i++) {
+    for (let j = i + 1; j < chips.length; j++) {
+      if (+chips[i].innerHTML > +chips[j].innerHTML) {
+        counter++;
+      }
+    }
+  }
+  let rowEmpty = Math.ceil((+empty.style.order)/Math.sqrt(count));
+
+  counter = counter + rowEmpty;
+  // console.log (`counter= ${counter}`);
+
+  if (Math.sqrt(count) % 2 === 0) {
+    if (counter % 2 === 0) {
+      console.log ('good!');
+      runTimer();
+      chipsHandler();
+      // return;
+    } else {
+      console.log ('bad!');
+      createNewGame(count);
+    }  
+  } else {
+    if (counter % 2 !== 0) {
+      console.log ('good!');
+      runTimer();
+      chipsHandler();
+      // return;
+    } else {
+      console.log ('bad!');
+      createNewGame(count);
+    } 
+  }
+
+}
+
+
+
+function addBestScore() {
+  totalTime = sec + min * 60;
+  score = {
+    'totaltime': totalTime,
+    'time':  `${currMin} : ${currSec}`,
+    'moves': movesCount,
+  }
+  if (localStorage.getItem('bestScores')) {
+    scores = JSON.parse(localStorage.getItem('bestScores'))
+  }
+  // console.log (scores);
+  scores.push(score);
+  if (scores.length >= 2) {
+    scores = scores.sort((a,b) => a['totaltime'] - b['totaltime']);
+  }
+  if (scores.length >= 10) {
+    scores.splice(10, scores.length - 1);
+  }
+  // console.log (scores);
+  console.log (`length  = ${scores.length}`);
+
+  localStorage.setItem('bestScores', JSON.stringify(scores));
+}
 
 
 
